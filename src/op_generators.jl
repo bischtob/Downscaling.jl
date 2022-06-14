0784f1b91943ed084218632955b4fb08f87770c8
bischtob <tobias.bischoff@pm.me>
2022-06-14 16:34:07 -0700

    uno

--- src/op_generators.jl
+++ src/op_generators.jl
@@ -4,121 +4,132 @@ using Functors
 using NeuralOperators
 
 """
-    OperatorUNetGenerator
+    UNetDiscriminator
 """
-struct OperatorUNetGenerator
-    initial
-    downblocks
-    resblocks
-    upblocks
-    final
+struct UNetDiscriminator
+    unet
+    knet
+end
+#         kx = self.knet(grid)
+        kx = kx.view(batch_size,-1, 1)
+        x = x.view(batch_size,-1, 1)
+        x = torch.einsum('bik,bik->bk', kx, x)/(res1*res2)
+
+
+@functor UNetDiscriminator
+
+function (op::UNetDiscriminator)(x)
+    return op.knet(op.unet(x))
 end
 
-@functor OperatorUNetGenerator
 
-function OperatorUNetGenerator(
+"""
+    UNetOperator
+"""
+struct UNetOperator
+    lifting
+    downsampling
+    nonlinear
+    upsampling
+    projection
+end
+
+@functor UNetOperator
+
+function UNetOperator(
     in_channels::Int,
-    num_features::Int=8,
-    num_residual::Int=4,
-    device=gpu,
-)
-    initial_layer = Chain(
-        OperatorConv(in_channels => num_features, (256, 256), FourierTransform, permuted=true),
-        InstanceNorm(num_features),
-        x -> relu.(x)
-    )
-
-    downsampling_blocks = [
-        OperatorConvBlock(num_features, num_features * 2, (128, 128), true, device),
-        OperatorConvBlock(num_features * 2, num_features * 4, (64, 64), true, device),
-    ]
-
-    resnet_blocks = Chain([OperatorResidualBlock(num_features * 4, (64, 64)) for _ in range(1, length=num_residual)]...)
-
-    upsampling_blocks = [
-        OperatorConvBlock(num_features * 4, num_features * 2, (128, 128), true, device),
-        OperatorConvBlock(num_features * 2, num_features, (256, 256), true, device),
-    ]
-    final_layer = Chain(
-        OperatorConv(num_features => in_channels, (256, 256), FourierTransform, permuted=true),
-    )
-
-    return OperatorUNetGenerator(
-        initial_layer,
-        downsampling_blocks,
-        resnet_blocks,
-        upsampling_blocks,
-        final_layer
-    ) |> device
-end
-
-function (net::OperatorUNetGenerator)(x)
-    input = net.initial(x)
-    for layer in net.downblocks
-        input = layer(input)
-    end
-    input = net.resblocks(input)
-    for layer in net.upblocks
-        input = layer(input)
-    end
-    return tanh.(net.final(input))
-end
-
-"""
-    OperatorConvBlock
-"""
-struct OperatorConvBlock
-    conv
-end
-
-@functor OperatorConvBlock
-
-function OperatorConvBlock(
-    in_channels::Int,
-    out_channels::Int,
-    modes::Tuple,
-    with_activation::Bool=true,
-    device=gpu;
+    num_features::Int,
+    num_modes::Tuple,
+    num_subsample::Int,
+    num_nonlinear::Int;
+    σ=gelu,
     kwargs...
 )
-    return ConvBlock(
-        Chain(
-            OperatorConv(in_channels => out_channels, modes, FourierTransform, permuted=true),
-            InstanceNorm(out_channels),
-            if with_activation
-                x -> relu.(x)
-            else
-                identity
-            end)
-    ) |> device
+    lifting = Chain(
+        Dense(in_channels, num_features),
+        x -> σ.(x),
+        Dense(num_features, num_features),
+    )
+
+    downsampling = [
+        OperatorBlock(num_features * 2^(i - 1), num_features * 2^i, div.(num_modes, Ref(2^i)), σ=σ, kwargs...) for i = 1:num_subsample
+    ]
+
+    nonlinear = Chain(
+        [OperatorBlock(num_features * 2^num_subsample, div.(num_modes, Ref(2^num_subsample)), σ=σ, kwargs...) for _ in range(1, length=num_nonlinear)]...
+    )
+
+    upsampling = [
+        OperatorBlock(num_features * 2^i, num_features * 2^(i - 1), div.(num_modes, Ref(2^i)), σ=σ, kwargs...) for i in reverse(1:num_subsample)
+    ]
+
+    projection = Chain(
+        Dense(num_features, num_features),
+        x -> σ.(x),
+        Dense(num_features, in_channels),
+    )
+
+    return UNetOperator(
+        lifting,
+        downsampling,
+        nonlinear,
+        upsampling,
+        projection,
+    )
 end
 
-function (net::OperatorConvBlock)(x)
-    return net.conv(x)
+function (op::UNetOperator)(x)
+    input = permutedims(x, (3, 2, 1, 4))
+    input = op.lifting(x)
+
+    ds_layers = []
+    for layer in op.downsampling
+        input = layer(input)
+        push!(ds_layers, input)
+    end
+
+    input = net.nonlinear(input)
+
+    for layer in net.upsampling
+        input = Flux.stack([layer(input), pop!(ds_layers)], dims=1)
+    end
+
+    input = op.projection(input)
+
+    return permutedims(input, (3, 2, 1, 4))
 end
 
+
 """
-    OperatorResidualBlock
+    OperatorBlock
 """
-struct OperatorResidualBlock
+struct OperatorBlock
     block
 end
 
-@functor OperatorResidualBlock
+@functor OperatorBlock
 
-function OperatorResidualBlock(
+function OperatorBlock(
     in_channels::Int,
-    modes::Tuple,
-    device=gpu
+    out_channels::Int,
+    modes::Tuple;
+    transform=FourierTransform,
+    σ=gelu,
+    permuted=false,
+    normed=false
 )
-    return OperatorResidualBlock(
+    return ConvBlock(
         Chain(
-            OperatorConvBlock(in_channels, in_channels, modes, true, device),
-            OperatorConvBlock(in_channels, in_channels, modes, false, device)
+            OperatorKernel(in_channels => out_channels, modes, transform, σ, permuted=permuted),
+            if normed
+                InstanceNorm(out_channels)
+            else
+                identity
+            end
         )
-    ) |> device
+    )
 end
 
-function (net::OperatorResidualBlock)(x)
-    return x + net.block(x)
+function (net::OperatorConvBlock)(x)
+    return net.block(x)
 end
