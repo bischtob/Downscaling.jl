4cc9df86fd4e26e10c4a3dd3a7fafebdd2f56feb
bischtob <tobias.bischoff@pm.me>
2022-06-23 12:33:15 -0700

    unet op works

--- examples/benchmark.jl
+++ examples/benchmark.jl
@@ -3,22 +3,18 @@ using BenchmarkTools
 import Downscaling: UNetOperator
 
 # benchmark UNetOperator
-img_size = 256
+img_size = 128
 batch_size = 1
-in_channels = 1
-num_features = 32
-num_modes = (128, 128)
-num_subsample = 3
-num_nonlinear = 1
-x = rand(Float32, in_channels, img_size, img_size, batch_size) |> gpu
+n_channel = 1
+n_codim = 32
+n_modes = 64
+x = rand(Float32, n_channel, img_size, img_size, batch_size) |> gpu
 op = UNetOperator(
-    in_channels,
-    num_features,
-    num_modes,
-    num_subsample,
-    num_nonlinear,
+    n_channel,
+    n_codim,
+    n_modes,
 ) |> gpu
-op(x);
+op(x) |> size
 
 # gradient check
 loss = () -> sum(op(x))
--- src/op_generators.jl
+++ src/op_generators.jl
@@ -4,7 +4,6 @@ using Functors
 using NeuralOperators
 using Statistics
 
-
 # """
 #     UNetOperatorGenerator
 # """
@@ -75,110 +74,57 @@ using Statistics
 #     return sigmoid.(mean(op.unet(x)))
 # end
 
-
 """
     UNetOperator
 """
 struct UNetOperator
-    lifting
-    downsampling
-    nonlinear
-    upsampling
-    projection
-    σ  
+    network
 end
 
 @functor UNetOperator
 
-function UNetOperator(
-    in_channels::Int,
-    num_features::Int,
-    num_modes::Tuple,
-    num_subsample::Int,
-    num_nonlinear::Int;
-    lowpass_factor = 4,
-    σ=gelu,
-    transform = FourierTransform,
-)
-    # lifting
-    lifting = []
-    push!(lifting, Dense(in_channels, div(num_features, 2)))
-    push!(lifting, Dense(div(num_features, 2), num_features))
+function UNetOperator(n_channel::Int, n_codim::Int, n_modes::Int; trafo=FourierTransform, σ=gelu)
+    @assert n_codim ÷ 2 > 0
+    @assert n_modes ÷ 64 > 0
 
-    # downsampling
-    downsampling = []
-    for i in 1:num_subsample
-        mode_contraction = div.(num_modes, Ref(lowpass_factor^i))
-        push!(downsampling, OperatorKernel(num_features * 2^(i - 1) => num_features * 2^i, mode_contraction, transform))
-    end
+    network = 
+        Chain(
+            Dense(n_channel, n_codim ÷ 2, σ),
+            SkipConnection(
+                Chain(
+                    Dense(n_codim ÷ 2, n_codim, σ),
+                    SkipConnection(
+                        Chain(
+                            OperatorKernel(n_codim=>2n_codim, (n_modes÷4, n_modes÷4), trafo, σ),
+                            SkipConnection(
+                                Chain(
+                                    OperatorKernel(2n_codim=>4n_codim, (n_modes÷16, n_modes÷16), trafo, σ),
+                                    SkipConnection(
+                                        Chain(
+                                            OperatorKernel(4n_codim=>8n_codim, (n_modes÷64, n_modes÷64), trafo, σ),
+                                            OperatorKernel(8n_codim=>8n_codim, (n_modes÷64, n_modes÷64), trafo, σ),
+                                            OperatorKernel(8n_codim=>4n_codim, (n_modes÷16, n_modes÷16), trafo, σ),
+                                        ),
+                                        vcat,
+                                    ),
+                                    OperatorKernel(8n_codim=>2n_codim, (n_modes÷4, n_modes÷4), trafo, σ),
+                                ),
+                                vcat,
+                            ),
+                            OperatorKernel(4n_codim=>n_codim, (n_modes, n_modes), trafo, σ),
+                        ),
+                        vcat,
+                    ),
+                    Dense(2n_codim, 3n_codim, σ),
+                ),
+                vcat,
+            ),
+            Dense(3n_codim + n_codim÷2, n_channel, σ),
+        )
 
-    # nonlinear bulk
-    channel_expansion = (num_features * 2^num_subsample => num_features * 2^num_subsample)
-    mode_contraction = div.(num_modes, Ref(lowpass_factor^num_subsample))
-    nonlinear = Chain(
-        [OperatorKernel(channel_expansion, mode_contraction, transform, σ) for _ in 1:num_nonlinear]...
-    )
-
-    # upsampling
-    upsampling = []
-    for i in reverse(1:num_subsample)
-        if i == num_subsample
-            channel_expansion = (num_features * 2^i => num_features * 2^(i-1))
-        else
-            #channel_expansion = (num_features * 2^(i+1) => num_features * 2^(i-1))
-            channel_expansion = (num_features * 2^i => num_features * 2^(i-1))
-        end
-        mode_contraction = div.(num_modes, Ref(lowpass_factor^(i-1)))
-        push!(upsampling, OperatorKernel(channel_expansion, mode_contraction, transform))
-    end
-
-    # projection
-    projection = []
-    push!(projection, Dense(2 * num_features, 3 * num_features))
-    push!(projection, Dense(3 * num_features + div(num_features, 2), in_channels))
-
-    return UNetOperator(
-        lifting,
-        downsampling,
-        nonlinear,
-        upsampling,
-        projection,
-        σ,
-    )
+    return UNetOperator(network)
 end
 
 function (op::UNetOperator)(x)
-    # lifting
-    l0 = op.σ.(op.lifting[1](x))
-    l1 = op.σ.(op.lifting[2](l0))
-
-    # downsampling and storing for bypass connections
-    tmp = l1
-    ds = []
-    for (idx, layer) in enumerate(op.downsampling)
-        tmp = op.σ.(layer(tmp))
-        if idx < length(op.downsampling)
-            push!(ds, tmp)
-        end
-    end
-
-    # nonlinear layers without down- or upsampling
-    tmp = op.nonlinear(tmp)
-
-    # upsampling with adding bypass connections
-    for (idx, layer) in enumerate(op.upsampling)
-        if idx > 1
-            bypass_idx = length(ds) - (idx - 1 - 1) 
-            #tmp = vcat([tmp, ds[bypass_idx]]...)
-        end
-        tmp = op.σ.(layer(tmp))
-    end
-
-    # projection
-    tmp = vcat([tmp, l1]...)
-    lm1 = op.σ.(op.projection[1](tmp))
-    lm1 = vcat([lm1, l0]...)
-    lm0 = op.σ.(op.projection[2](lm1))
-
-    return lm0
+    return op.network(x)
 end
